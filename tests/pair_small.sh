a734e5c83729bcee99c2776a11a16ae96c14cab1
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-08-12 11:53:32 -0400

    Update small paired test to generalize it and move test data to 'galaxy/tst-data

--- tests/pair_small.sh
+++ tests/pair_small.sh
@@ -4,10 +4,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export PATH=${SCRIPT_DIR}/../../conda/bin:${PATH}
 pwd;hostname;date
 
-DATA="data/test/test_exons.gtf"
+TEST="pair_small"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
 
-CMD="python src/eventanalysis/event_analysis.py -o testout/pair_small $* ${DATA}"
+DATA="test_exons.gtf"
+
+CMD="trand -o testout/pair_small $* ${INPUT_DIR}/${DATA}"
 echo "CMD: ${CMD}"
 eval "${CMD}"
 
 date
+echo "### Finished test: ${TEST} on $(date)"
