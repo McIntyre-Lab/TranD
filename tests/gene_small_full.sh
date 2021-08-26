893ce7389c7516017debef41a8c6360a3405f839
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-08-26 13:50:16 -0400

    Reorganize small synthetic tests

--- tests/gene_small_full.sh
+++ tests/gene_small_full.sh
@@ -4,18 +4,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export PATH=${SCRIPT_DIR}/../../conda/bin:${PATH}
 pwd;hostname;date
 
-TEST="two_gtf_pairwise_small"
+TEST="gene_small_full_gene"
 TESTDIR="testout/${TEST}"
 INPUT_DIR="galaxy/test-data"
 OUTPUT_DIR=$TESTDIR
 rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
 echo "### Starting test: ${TEST}"
 
-DATA="${INPUT_DIR}/test_set_B73_END.gtf ${INPUT_DIR}/test_set_B73_R.gtf"
+DATA="test_exons_gene.gtf"
 
-CMD="trand -o ${OUTPUT_DIR} $* ${DATA}"
+CMD="trand -f -e gene -o ${OUTPUT_DIR} $* ${INPUT_DIR}/${DATA}"
 echo "CMD: ${CMD}"
-time eval "${CMD}"
+eval "${CMD}"
 
 date
-echo "### Finished test: ${TEST} on $(date)"
