9ec1763535ccd8f8c8b61c58cf1bb9d4b0bc7b22
Oleksandr Moskalenko <om@rc.ufl.edu>
2021-09-09 18:43:04 -0400

    Add a single file small gene test with skip intermediate files

---
+++ tests/gene_small_full_skip_interm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export PATH=${SCRIPT_DIR}/../../conda/bin:${PATH}
+pwd;hostname;date
+
+TEST="gene_small_full_gene_skip_interm"
+TESTDIR="testout/${TEST}"
+INPUT_DIR="galaxy/test-data"
+OUTPUT_DIR=$TESTDIR
+rm -rf "${TESTDIR}"
+mkdir -p "${TESTDIR}"
+echo "### Starting test: ${TEST}"
+
+DATA="test_exons_gene.gtf"
+
+CMD="trand -f -e gene -o ${OUTPUT_DIR} -i -s $* ${INPUT_DIR}/${DATA}"
+echo "CMD: ${CMD}"
+eval "${CMD}"
+
+date
