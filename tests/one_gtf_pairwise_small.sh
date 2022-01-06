7732d5b85970222588f9d4f5a92a4904e4c4eda6
Oleksandr Moskalenko <om@rc.ufl.edu>
2022-01-06 13:09:59 -0500

    Add a cprofile run

--- tests/one_gtf_pairwise_small.sh
+++ tests/one_gtf_pairwise_small.sh
@@ -14,7 +14,11 @@ echo "### Starting test: ${TEST}"
 
 DATA="test_exons.gtf"
 
-CMD="trand -f -s -o ${OUTPUT_DIR} $* ${INPUT_DIR}/${DATA}"
+#Performance profiling
+EXE="${HOME}/projects/bioinformatics/mcintyre-cid/TranD/conda/bin/trand"
+CMD="python -m cProfile -o ${OUTPUT_DIR}/cprofile.log ${EXE} -f -s -o ${OUTPUT_DIR} $* ${INPUT_DIR}/${DATA}"
+#standard test
+#CMD="trand -f -s -o ${OUTPUT_DIR} $* ${INPUT_DIR}/${DATA}"
 echo "CMD: ${CMD}"
 time eval "${CMD}"
 
