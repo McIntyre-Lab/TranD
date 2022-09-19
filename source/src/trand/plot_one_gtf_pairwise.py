18666ad0f0e1871ec6163fc7633f354cab179f5d
Adalena Nanni <adalena.nanni@ufgi-grimshawi.ahc.ufl.edu>
2022-09-19 15:29:03 -0400

    Fix typo in 1GTF pairwise plot prefix addition

--- source/src/trand/plot_one_gtf_pairwise.py
+++ source/src/trand/plot_one_gtf_pairwise.py
@@ -24,10 +24,11 @@ def plot_one_gtf_pairwise(outdir, td_data, prefix):
     """
 # Plot 1
     # Upset plot for the number of genes with each kind of alternative splicing (AS)
-    PF.plot_gene_AS_upset(td_data, "{}/gene_AS_upset.rtf".format(outdir))
     if prefix is not None:
-        plt.savefig("{}{}_/gene_AS_upset.png".format(outdir, prefix), dpi=600, format="png")
+        PF.plot_gene_AS_upset(td_data, "{}/{}_gene_AS_upset.rtf".format(outdir, prefix))
+        plt.savefig("{}/{}_gene_AS_upset.png".format(outdir, prefix), dpi=600, format="png")
     else:
+        PF.plot_gene_AS_upset(td_data, "{}/gene_AS_upset.rtf".format(outdir))
         plt.savefig("{}/gene_AS_upset.png".format(outdir), dpi=600, format="png")
     plt.clf()
 
@@ -35,9 +36,10 @@ def plot_one_gtf_pairwise(outdir, td_data, prefix):
     # Upset plot the number of transcript pairs with each kind of
     #   AS for a single transcriptome with added box plots
     #   of the number of nt different and the proportion of nt different
-    PF.plot_pair_AS_upset_nt_box(td_data, "{}/transcript_pair_AS_upset_nt_boxplot.rtf".format(outdir))
     if prefix is not None:
+        PF.plot_pair_AS_upset_nt_box(td_data, "{}/{}_transcript_pair_AS_upset_nt_boxplot.rtf".format(outdir, prefix))
         plt.savefig("{}/{}_transcript_pair_AS_upset_nt_boxplot.png".format(outdir, prefix), dpi=600, format="png")
     else:
+        PF.plot_pair_AS_upset_nt_box(td_data, "{}/transcript_pair_AS_upset_nt_boxplot.rtf".format(outdir))
         plt.savefig("{}/transcript_pair_AS_upset_nt_boxplot.png".format(outdir), dpi=600, format="png")
     plt.clf()
