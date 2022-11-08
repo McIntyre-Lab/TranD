394b7b928297f77845de081841fb08212406fe72
Adalena Nanni <adalena.nanni@ufgi-grimshawi.ahc.ufl.edu>
2022-11-08 14:55:21 -0500

    Update utilities for gene_id correction after gffcompare and subsetting pairwise output by transcript_id list(s)

--- utilities/subset_trand_pairwise_transcript_distance.py
+++ utilities/subset_trand_pairwise_transcript_distance.py
@@ -8,7 +8,7 @@ def getOptions():
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         description=(
-            "Subset TranD pairwise transcript distance file by list of genes."
+            "Subset TranD pairwise transcript distance file by list of genes or transcripts."
         )
     )
 
@@ -27,31 +27,45 @@ def getOptions():
         "--list-type",
         dest="inType",
         required=False,
-        choices=['gene_id'],
+        choices=['gene_id', 'transcript_id'],
         default='gene_id',
         help=(
-            "Type of feature in inclusion/exclusion list provided "
-            "(currently only gene_id"
+            "Type of feature in inclusion/exclusion list provided. "
+            "Using transcript_id will check for transcript_id values in BOTH "
+            "transcript_1 AND transcript_2 columns if 1 list provided. If two "
+            "lists provided for transcript_id then first list will be compared "
+            "against transcript_1 and second against transcript_2 of pariwise "
+            "file. Default: gene_id."
         )
     )
     parser.add_argument(
         "-i",
         "--include-list",
         dest="inInclude",
+        action="append",
         required=False,
         help=(
-            "List of gene IDs to include in output from input "
-            "pairwise distance TranD file (no header) - cannot be used with exclusion list"
+            "List of gene (or transcript) IDs to include in output from "
+            "pairwise distance TranD file. List should have no header. "
+            "Cannot be used with exclusion list. The -i argument can be used "
+            "twice to give two separate lists of transcript_id values. "
+            "The first will be compared to transcript_1 and the second to "
+            "transcript_2 in the pairwise file."
         )
     )
     parser.add_argument(
         "-e",
         "--exclude-list",
         dest="inExclude",
+        action="append",
         required=False,
         help=(
-            "List of gene IDs to exclude in output from input "
-            "pairwise distance TranD file (no header) - cannot be used with inclusion list"
+            "List of gene (or transcript) IDs to exclude in output from "
+            "pairwise distance TranD file. List should have no header. "
+            "Cannot be used with inclusion list. The -i argument can be used "
+            "twice to give two separate lists of transcript_id values. "
+            "The first will be compared to transcript_1 and the second to "
+            "transcript_2 in the pairwise file."
         )
     )
 
@@ -77,14 +91,36 @@ def main():
     
     # Check that only one list argument was provided
     if args.inInclude is None and args.inExclude is None:
-        print("ERROR: No list provided to subset pairwise file on")
+        print("ERROR: No list provided to subset pairwise file on.")
         sys.exit()
     elif args.inInclude is not None and args.inExclude is None:
         include = True
-        idDF = pd.read_csv(args.inInclude, names=[listVar])
+        if len(args.inInclude) == 1:
+            idDF = pd.read_csv(args.inInclude[0], names=[listVar])
+        elif len(args.inInclude) == 2:
+            if listVar == "transcript_id":
+                t1DF = pd.read_csv(args.inInclude[0], names=[listVar])
+                t2DF = pd.read_csv(args.inInclude[1], names=[listVar])
+            else:
+                print("ERROR: -t must be transcript_id when more than one list provided.")
+                sys.exit()
+        else:
+            print("ERROR: A maximum of two lists can be provided as input.")
+            sys.exit()
     elif args.inInclude is None and args.inExclude is not None:
         include = False
-        idDF = pd.read_csv(args.inExclude, names=[listVar])
+        if len(args.inExclude) == 1:
+            idDF = pd.read_csv(args.inExclude[0], names=[listVar])
+        elif len(args.inExclude) == 2:
+            if listVar == "transcript_id":
+                t1DF = pd.read_csv(args.inExclude[0], names=[listVar])
+                t2DF = pd.read_csv(args.inExclude[1], names=[listVar])
+            else:
+                print("ERROR: -t must be transcript_id when more than one list provided.")
+                sys.exit()
+        else:
+            print("ERROR: A maximum of two lists can be provided as input.")
+            sys.exit()
     else:
         print("ERROR: Cannot use both inclusion and exclusion list - only provide one or the other")
         sys.exit()
@@ -92,11 +128,27 @@ def main():
     if include:
         # If list is for inclusion
         # Extract rows of pairwise distance that have a value contained in list of IDs
-        filterDF = tdDF[tdDF[listVar].isin(idDF[listVar])]
+        if listVar == "gene_id":
+            filterDF = tdDF[tdDF[listVar].isin(idDF[listVar])]
+        else:
+            if len(args.inInclude) == 1:
+                # A pair is retained if transcript_1 and transcript_2 are BOTH in the list
+                filterDF = tdDF[(tdDF["transcript_1"].isin(idDF[listVar])) & (tdDF["transcript_2"].isin(idDF[listVar]))]
+            else:
+                # A pair is retained if transcript_1 is in list 1 and transcript_2 is in list 2
+                filterDF = tdDF[(tdDF["transcript_1"].isin(t1DF[listVar])) & (tdDF["transcript_2"].isin(t2DF[listVar]))]
     else:
         # If list is for exclusion
         # Extract GTF elements that do NOT have a value contained in list of IDs
-        filterDF = tdDF[~tdDF[listVar].isin(idDF[listVar])]
+        if listVar == "gene_id":
+            filterDF = tdDF[~tdDF[listVar].isin(idDF[listVar])]
+        else:
+            if len(args.inInclude) == 1:
+                # A pair is retained if transcript_1 AND transcript_2 are NOT in the list
+                filterDF = tdDF[(~tdDF["transcript_1"].isin(idDF[listVar])) & (~tdDF["transcript_2"].isin(idDF[listVar]))]
+            else:
+                # A pair is retained if transcript_1 is NOT in list 1 and transcript_2 is NOT in list 2
+                filterDF = tdDF[(~tdDF["transcript_1"].isin(t1DF[listVar])) & (~tdDF["transcript_2"].isin(t2DF[listVar]))]
         
     # Output filtered file
     filterDF.to_csv(args.outFile, index=False)
