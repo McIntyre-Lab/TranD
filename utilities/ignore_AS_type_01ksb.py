d242364fb5cddfe9276805607635b19cbdebc1ca
KSB54 <kinfebankole@gmail.com>
2023-01-31 14:58:00 -0500

    setup directories, create script base
    added directories for the output data (datavis) and created empty script in utilities

---
+++ utilities/ignore_AS_type_01ksb.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 12:55:09 2023
+
+@author: knife
+"""
+
+# new function: ignore any AS type to create a customizable plot
+# removing all should just show a grey dot w number of pairs
+# default is to show all
