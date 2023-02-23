028050b38baaf00013d3b17a4bbda8d41c75d679
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2023-02-23 15:55:20 -0500

    setup utility

--- utilities/id_ER_set_01ksb.py
+++ utilities/id_ER_set_01ksb.py
@@ -10,18 +10,3 @@ Created on Thu Feb 23 14:43:44 2023
 Identify possible ER sets of a 1 GTF or 2 GTF Pairwise File
 
 """
-
-import argparse
-
-def getOptions():
-        #arg parse
-        
-        parser = argparse.ArgumentParser()
-        
-        args = parser.parse_args()
-        return args
-
-def logicFunction():
-        #rename this
-        return 'empty' #csv or data file
-
