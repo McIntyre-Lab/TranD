e0f92aa2a22f8f6edb98bcff53fea8d4d5c66980
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2023-02-28 12:35:41 -0500

    added getOptions for user input via argparse, added single gene test data to testdata

--- utilities/id_ER_set_01ksb.py
+++ utilities/id_ER_set_01ksb.py
@@ -7,6 +7,79 @@ Created on Thu Feb 23 14:43:44 2023
 """
 
 """
-Identify possible ER sets of a 1 GTF or 2 GTF Pairwise File
+Identify possible ER sets using TRAND ouptput of a 1 or 2 GTF pairwise file 
 
 """
+
+import argparse
+
+def getOptions():
+        """
+        
+        Function to store user input via argparse
+
+        Returns
+        -------
+        args : ARGPARSE ARGUMENTS
+                User input via argparse.
+
+        """
+        
+        parser = argparse.ArgumentParser(description="Output a csv containing information on "
+                                         "the ER sets for a set of transcripts using TRAND output "
+                                         "data. Contains the option to include or exclude "
+                                         "transcripts with intron retention events (--includeIR). "
+                                         "Input a TRAND output file (csv) (--indir), "
+                                         "IR inclusion (--includeIR), and an output path (--outdir)."
+                                         )
+        
+        # INPUT
+        parser.add_argument(
+                "-i",
+                "--indir",
+                dest="indir",
+                required=True,
+                help="Location of TRAND output file")
+        
+        parser.add_argument(
+                "-ir",
+                "--includeIR",
+                dest="includeIR",
+                required=True,
+                default='Y',
+                const='Y',
+                nargs='?',
+                choices=['Y','N'],
+                help="Choose N to exclude transcript models with intron retention events from ER sets.")
+        
+        # OUTPUT
+        parser.add_argument(
+                "-o",
+                "--outdir",
+                dest="outdir",
+                required=True,
+                help="Location of output directory, created if missing")
+        
+        parser.add_argument(
+                "-f",
+                "--force",
+                dest="force",
+                action="store_true",
+                help="Force overwrite existing output directory and files within")
+        
+        args = parser.parse_args()
+        return args
+
+# input dataframe and chop. (input is a full csv file)
+# necessary columns: t1, t2, flag_IR, prop_ER
+# -> new dataframe: t1, t2, flag_ER (with all IR removed if excludeIR = true)
+
+
+def main():
+        return 'KSB'
+
+if __name__ == '__main__':
+        global args
+        args = getOptions()
+        main()
+        
