61f75cbb9eff80ae14e501dd29940654464311a4
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2023-05-11 10:16:33 -0400

    add pretty version of utility to be updated

---
+++ utilities/id_ujc_01ksb.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 09:31:01 2023
+
+@author: k.bankole
+"""
+
+"""
+
+Identify the unique junction chains (UJC) of a GTF file and combine
+transcripts into UJCs.
+
+Created from a previous utility in TranD named consolidation
+
+Version 1
+
+"""
