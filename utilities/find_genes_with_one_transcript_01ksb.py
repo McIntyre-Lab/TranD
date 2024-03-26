0f9a6f5f5ee9e0ba8433a723ba8423efb1798090
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2024-03-26 17:57:03 -0400

    added output files to one gene one transcript utiltity

--- utilities/find_genes_with_one_transcript_01ksb.py
+++ utilities/find_genes_with_one_transcript_01ksb.py
@@ -2,10 +2,13 @@
 
 import argparse
 import trand.io
+import os
+import pandas as pd
 
 def getOptions():
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="Read in GTF. Output list of unique genes (list_{name}_uniq_gene_set) and list of genes with one "
+                                     "transcript (list_{name}_genes_w_one_xscript.csv.")
     
     # Input data
     parser.add_argument("-g",
@@ -15,30 +18,64 @@ def getOptions():
                         help="GTF to find genes with one transcript")
     
     # Output data
-    parser.add_argument("-", "--", dest="", required=True, help="")
+    parser.add_argument("-o",
+                        "--output-directory",
+                        dest="outDir",
+                        required=True,
+                        help="Output directory. Must already exist.")
+    
+    # parser.add_argument("-p",
+    #                     "--prefix",
+    #                     dest="outDir",
+    #                     required=True,
+    #                     help="Output directory. Must already exist.")
+    
     
     args = parser.parse_args()
     return args
 
 def main():
     
-    inGTF = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/fiveSpecies_annotations/fiveSpecies_2_dmel6_ujc.gtf"
+    # inGTF = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/fiveSpecies_annotations/fiveSpecies_2_dmel6_ujc.gtf"
+    outDir = "/nfshome/k.bankole/Desktop"
+    
+    inGTF = args.inGTF
+    outDir = args.outDir
+    
+    prefix = os.path.basename(inGTF).split('.')[0]
+
     inDf = trand.io.read_exon_data_from_file(inGTF)
     
+    
+    # Group by gene, count number of unique genes
     geneDf = inDf.groupby('gene_id').agg({'transcript_id':set}).reset_index()
-    
     uniqGeneSet = set(geneDf['gene_id'].to_list())
-    
     print("Total number of unique genes: {}".format(len(uniqGeneSet)))
     
+    # Subset for genes with only one transcript
     oneXscriptSet = set(geneDf['gene_id'][geneDf['transcript_id'].apply(lambda x: len(x) == 1)].to_list())
-    
     print ("Number of genes with one transcript: {}".format(len(set(oneXscriptSet))))    
     print ("Number of genes with more than one transcript (should match number of genes in TranD output): {}".format(len(uniqGeneSet) - len(oneXscriptSet)))
     
+    # Output both lists
     
-    # Missing output files...
-
+    outGeneDf = pd.DataFrame(geneDf['gene_id'])
+    outGeneDf = outGeneDf.rename({'gene_id':'geneID'})
+    
+    outOneDf = pd.DataFrame({'geneID':list(oneXscriptSet)}).sort_values(by='geneID').reset_index(drop=True)
+    
+    geneLstFile = outDir + "/list_{}_uniq_gene_set.csv".format(prefix)
+    oneListFile = outDir + "/list_{}_genes_w_one_xscript.csv".format(prefix)
+    
+    try:
+            outGeneDf.to_csv(geneLstFile, index=False)
+            outOneDf.to_csv(oneListFile, index=False)
+    except OSError:
+            raise OSError("Output directory must already exist.")
+    
+    
+    
+    
 if __name__ == '__main__':
     # Parse command line arguments
     global args
