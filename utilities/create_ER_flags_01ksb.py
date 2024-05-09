e84e6dacb7ca0ed15bedd012a8ec12fd4d0a71c6
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2024-05-09 14:05:36 -0400

    completed script that flags which exon regions from the gene that a transcript has

--- utilities/create_ER_flags_01ksb.py
+++ utilities/create_ER_flags_01ksb.py
@@ -19,7 +19,10 @@ def getOptions():
         """
         
         # Parse command line arguments
-        parser = argparse.ArgumentParser(description="Identifies groups of transcripts that share all of their exon regions.")
+        parser = argparse.ArgumentParser(description="Uses \"event_analysis_er.csv\" from TranD 1 GTF gene mode"
+                                         "to, per gene, flag what exon regions a transcript has. "
+                                         "Outputs a CSV in stacked form (one row per "
+                                         "exon region per transcript.")
         
         # INPUT
         parser.add_argument(
@@ -30,19 +33,12 @@ def getOptions():
                 help="Location of event analysis ER file from gene mode output"
                 )
         
-        
         # OUTPUT
-        # parser.add_argument("-o",
-        #                     "--output-gtf",
-        #                     dest="outGTF", 
-        #                     required=True,
-        #                     help="Path and filename for output GTF")
-        
         parser.add_argument("-o",
-                            "--output-directory",
-                            dest="outDir", 
+                            "--output-csv",
+                            dest="outCSV", 
                             required=True,
-                            help="Output path")
+                            help="Output file path")
         
         args = parser.parse_args()
         return args
@@ -56,61 +52,58 @@ def main():
     # eaFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/test_gene_based_ERGs/sub_ea_LOC110191367_noIR.csv"
     # eaFile = "{}/sub_keepIR_event_analysis_er.csv".format(PROJ)
     # eaFile = "/nfshome/k.bankole/mclab/SHARE/McIntyre_Lab/sex_specific_splicing/trand_1GTF_geneMode_fiveSpecies_ujc/fiveSpecies_2_dser1_ujc_event_analysis_er.csv"
-    # pw_eaFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/test_gene_based_ERGs/sub_pw_event_analysis.csv"
-    # pw_eaFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/test_gene_based_ERGs/sub_pw_noIR_event_analysis.csv"    
-    
-    # outGTF = "{}/gene_based_erg_keepIR.gtf".format(PROJ)
-    outGTF = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/test_gene_based_ERGs/gene_based_erg_noIR.gtf"
-    
-    outDir = ""
+    # outFile = "/nfshome/k.bankole/mclab/SHARE/McIntyre_Lab/sex_specific_splicing/test_ER_flags/test_ER_flags.csv"
     
     eaFile = args.eaFile
-    # outGTF = args.outGTF    
-    outDir = args.outDir
-    
+    outFile = args.outFile
+
     eaDf = pd.read_csv(eaFile, low_memory=False)       
     
-    tmpDf = eaDf[['er_id','er_transcript_ids','gene_id']].copy()
-
-    # testRowLst = [
-    # {'er_id': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268_exon_1', 'er_transcript_ids': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268', 'gene_id': 'LOC110176799'},
-    # {'er_id': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268_exon_2', 'er_transcript_ids': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268', 'gene_id': 'LOC110176799'},
-    # {'er_id': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268_exon_3', 'er_transcript_ids': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268', 'gene_id': 'LOC110176799'},
-    # {'er_id': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268_exon_4', 'er_transcript_ids': '0c10a9919a9cca1fcfd6a40a3e1884251ab221ec924581fdd4ca46a88f951268', 'gene_id': 'LOC110176799'},
-    # ]
-    # tmpDf = pd.concat([tmpDf, pd.DataFrame(testRowLst)])
+    infoDf = eaDf[['er_id','er_transcript_ids','gene_id']].copy()
     
-    tmpDf.columns = ['ER','transcripts','geneID']
-    tmpDf['transcripts'] = tmpDf['transcripts'].str.split('|')
+    infoDf.columns = ['ER','jxnHash','geneID']
+    infoDf['jxnHash'] = infoDf['jxnHash'].str.split('|')
+    infoDf = infoDf.explode('jxnHash')
     
-    tmpDf = tmpDf.explode('transcripts')
+    geneDct = dict(infoDf.groupby('geneID').apply(lambda x: set(x['ER'])))
+    xscriptDct = dict(infoDf.groupby('jxnHash').apply(lambda x: set(x['ER'])))
     
-    perGeneDf = tmpDf.groupby('geneID')
-    
-    dfLst = []
-    wideDfLst = []
-    
-    alphatic = time.perf_counter()
-    for gene, infoDf in perGeneDf:
+    xscriptLst = []
+    geneLst = []
+    erLst = []
+    flagLst = []
+    for row in infoDf.to_dict('records'):
         
-        infoDf['flag_ERPresent'] = 1
-        wideDf = pd.pivot_table(infoDf, values='flag_ERPresent', index=['transcripts','geneID'], columns='ER', fill_value=0)
+        gene = row['geneID']
+        transcript = row['jxnHash']
         
-        wideDf.columns = sorted(wideDf.columns, key=lambda x: int(x.split("ER")[1]) if 'ER' in x else int(x.split("exon_")[1]))
-                
-        dfLst.append(wideDf.stack().reset_index(name='flag_ERPresent'))
-        wideDfLst.append(wideDf)
+        geneERSet = geneDct.get(gene)
+        
+        geneERSet = sorted(geneERSet, key=lambda x: int(x.split("ER")[1]) if 'ER' in x else int(x.split("exon_")[1]))
+        
+        xscriptERSet = xscriptDct.get(transcript)
+        
+        for exonRegion in geneERSet:
+            
+            if exonRegion in xscriptERSet:
+                flag = 1 
+            else:
+                flag = 0
+            
+            xscriptLst.append(transcript)
+            geneLst.append(gene)
+            erLst.append(exonRegion)
+            flagLst.append(flag)
     
-    outDf = pd.concat(dfLst)
+    outDf = pd.DataFrame({
+        'jxnHash':xscriptLst,
+        'geneID':geneLst,
+        'exonRegion':erLst,
+        'flag_ERPresent':flagLst
+    })
     
-    hmm = pd.concat(wideDfLst).fillna(0)
+    outDf.to_csv(outFile,index=False)
     
-    omegatoc = time.perf_counter()
-    print(f"Complete! Took {omegatoc-alphatic:0.4f} seconds. Doing next process...")
-
-    outDf.to_csv('{}/test_stacked.csv'.format(outDir),index=False)
-    hmm.to_csv('{}/test_wide.csv'.format(outDir),index=False)
-
 
 if __name__ == '__main__':
     # Parse command line arguments
