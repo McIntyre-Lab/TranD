cacfffe28ae07152e51861761151710c5607401c
Kinfeosioluwa Bankole <k.bankole@ufgi-vulcana.ahc.ufl.edu>
2024-05-16 15:54:08 -0400

    fixed a bug that occurred when genes appeared only in the data.

--- utilities/potential_winning_script_01ksb.py
+++ utilities/potential_winning_script_01ksb.py
@@ -10,6 +10,7 @@ import argparse
 import pandas as pd
 import trand.io
 import time
+import numpy as np
 
 def getOptions():
         """
@@ -56,58 +57,91 @@ def getOptions():
 
 def main():
     
-    erFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/trand_2gtf_fiveSpecies_er_vs_data/FBgn0000662_fiveSpecies_2_dmel6_ujc_er.gtf"
-    dataFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/trand_2gtf_fiveSpecies_er_vs_data/FBgn0000662_data.gtf"    
+    # erFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/trand_2gtf_fiveSpecies_er_vs_data/FBgn0000662_fiveSpecies_2_dmel6_ujc_er.gtf"
+    # dataFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/trand_2gtf_fiveSpecies_er_vs_data/FBgn0000662_data.gtf"    
+    
+    
+    erFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/trand_2gtf_fiveSpecies_er_vs_data/fiveSpecies_2_dmel6_ujc_er.gtf"
+    dataFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/dmel_sexdet_fiveSpecies_vs_data/mel_2_dmel6_uniq_jxnHash_sexDet.gtf"
     
     
     erFile = args.erFile
     dataFile = args.dataFile
     
     alphatic = time.perf_counter()
+    
     geneDf = trand.io.read_exon_data_from_file(erFile)
     dataDf = trand.io.read_exon_data_from_file(dataFile)
     
     geneDf = geneDf[['gene_id','seqname','start','end','strand']]
-    geneDf['ER'] = geneDf['gene_id'] + ':ER' + (geneDf.index + 1).astype(str)
+    
+    geneDf = geneDf.sort_values(['seqname','gene_id','start'])
+    
+    geneDf['ER'] = geneDf['gene_id'] + ':ER' + (geneDf.groupby('gene_id').cumcount() + 1).astype(str)
     geneDct = dict(geneDf.groupby('gene_id').apply(lambda x: sorted(set(x['ER']), key=lambda x: int(x.split("ER")[1]) if 'ER' in x else int(x.split("exon_")[1]))))
     
     # TODO: CHECK THAT ALL SETS ARE OF SIZE ONE
     erDct = geneDf.groupby('ER').agg('first').to_dict(orient='index')
     
+    
+    # row = pd.DataFrame({'gene_id':'test','seqname':'test','start':0,'end':0,'strand':13341}, index=[len(dataDf)+1])
+    # row2 = pd.DataFrame({'gene_id':'test','seqname':'test','start':0,'end':0,'strand':13341}, index=[len(dataDf)+2])
+    # row3 = pd.DataFrame({'gene_id':'test','seqname':'test','start':0,'end':0,'strand':13341}, index=[len(dataDf)+3])
+    # yourBoat = pd.DataFrame({'gene_id':'test','seqname':'test','start':0,'end':0,'strand':13341}, index=[len(dataDf)+4])
+    
+    # dataDf = pd.concat([dataDf,row,row2,row3,yourBoat])
+    
+    dataOnlyLst = []
+    
     records = dataDf.to_dict('records')
+    
     for row in records:
         gene = row['gene_id']
         # jxnHash = row['transcript_id']
         
         matchingERIDLst = []
         
-        for erID in geneDct.get(gene):
-            # print(erID)
-            erInfo = erDct.get(erID)
-            # print(erInfo)
-    
-            # print("looping...")
-            
-            if max(row['start'], erInfo['start']) < min(row['end'], erInfo['end']):
-                # print ("Yippee!")
-                # print(row)
+        if gene in geneDct.keys():
+            for erID in geneDct.get(gene):
                 # print(erID)
+                erInfo = erDct.get(erID)
                 # print(erInfo)
-                matchingERIDLst.append(erID)
+        
+                # print("looping...")
                 
-                
-        if matchingERIDLst:
-            row['ER(s)'] = matchingERIDLst
+                if max(row['start'], erInfo['start']) < min(row['end'], erInfo['end']):
+                    # print ("Yippee!")
+                    # print(row)
+                    # print(erID)
+                    # print(erInfo)
+                    
+                    matchingERIDLst.append(erID)
+            if matchingERIDLst:
+                row['ER(s)'] = matchingERIDLst
+            else:
+                row['ER(s)'] = "{}:{}_{}".format(gene,row['start'], row['end'])
         else:
-            row['ER(s)'] = "{}_{}".format(row['start'], row['end'])
+            dataOnlyLst.append(row)
+                    
     
-    test = pd.DataFrame(records)
+    dataWithERDf = pd.DataFrame(records).dropna(subset=['ER(s)'])
+    
+    
+    # Output data only gene list (remove from existing dataframe)
+    
+    # What if the gene only appears in the reference? does that matter? 
     
     omegatoc = time.perf_counter()
     
-    print(f"Complete! Took {(omegatoc-alphatic)/60:0.2f} seconds.")    
+    print(f"Complete! Took {(omegatoc-alphatic):0.4f} seconds.")    
     
-    dataDf.groupby('gene_id')
+    
+    # May not be true but makes sense right? if there is an exon that overlaps two exon regions
+    # it may not be true biological IR but its definitely overlapping an intron...
+    
+    # dataWithERDf['flag_IR'] = dataWithERDf.apply(1 if len(dataWithERDf['ER(s)']) > 1 else 0)
+    
+    # dataDf.groupby('gene_id')
     
 
 if __name__ == '__main__':
