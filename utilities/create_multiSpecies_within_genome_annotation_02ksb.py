f95ab2481cc9938c84c8977e6dd9f170f12dabfc
Kinfeosioluwa Bankole <k.bankole@ufgi-grimshawi.ahc.ufl.edu>
2024-07-17 19:10:02 -0400

    update script

--- utilities/create_multiSpecies_within_genome_annotation_02ksb.py
+++ utilities/create_multiSpecies_within_genome_annotation_02ksb.py
@@ -46,26 +46,101 @@ def main():
     # Parse command line arguments
 
     genomeName = "dmel6"
-    inCat = ["/blue/mcintyre/share/sex_specific_splicing/annotation_id_ujc_output/dmel650_2_dmel6_ujc.gtf",
-             "/blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsan11_2_dsan1_ujc_2_dmel6_noGeneID_ujc.gtf",
-             "/blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsim202_2_dsim2_ujc_2_dmel6_noGeneID_ujc.gtf",
-             "/blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsimWXD_2_dsim2_ujc_2_dmel6_noGeneID_ujc.gtf",
-             "/blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dyak21_2_dyak2_ujc_2_dmel6_noGeneID_ujc.gtf",
-             "/blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dser11_2_dser1_ujc_2_dmel6_noGeneID_ujc.gtf"]
+    inCatLst = ["/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/annotation_id_ujc_output/dmel650_2_dmel6_ujc.gtf",
+             "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsim202_2_dsim2_ujc_2_dmel6_noGeneID_ujc.gtf",
+             "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsan11_2_dsan1_ujc_2_dmel6_noGeneID_ujc.gtf",
+             "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dsimWXD_2_dsim2_ujc_2_dmel6_noGeneID_ujc.gtf",
+             "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dyak21_2_dyak2_ujc_2_dmel6_noGeneID_ujc.gtf",
+             "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/mapped_ujc_id_ujc_output/dser11_2_dser1_ujc_2_dmel6_noGeneID_ujc.gtf"]
 
     outGTF = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/sex_specific_splicing/fiveSpecies_annotations/fiveSpecies_2_dmel6_ujc_roz.gtf"
 
-    # inFolder = r"\\exasmb.rc.ufl.edu\blue\mcintyre\share\sex_specific_splicing\mapped_ujc_id_ujc_output"
+    # genomeName = args.genomeName
+    # inCatLst = args.inCat
+    # outGTF = args.outGTF
 
-    genomeName = args.genomeName
-    inCat = args.inCat
-    outGTF = args.outGTF
+
+    # TODO: keep track of number of exons for each transcript to check    
+        
+    gtfDct = dict()
+    for file in inCatLst:
+        #do things
+        fileName = os.path.basename(file)
+        print("Reading file: {}".format(fileName))
     
-    print(inCat)
-    for file in inCat:
-        print(file)
+        annoName = fileName.split('_2')[0].rstrip('_')
+        df = trand.io.read_exon_data_from_file(file)
+        gtfDct[annoName] = df
+    
+    
+    allHashSet = set()
+    masterDf = pd.DataFrame()
+    masterDf = list(gtfDct.values())[0]
+    gtf = list(gtfDct.values())[1]
+    
+    testDf = masterDf[masterDf['transcript_id'] == "000ab5cb90829f133fd4e78f093a2d795bc47c83a7aaef93743560cf7c37f949"]
+    test2Df = gtf[gtf['transcript_id'] == "000ab5cb90829f133fd4e78f093a2d795bc47c83a7aaef93743560cf7c37f949"]
+    
+    # group by transcript and merge............
+    merge = pd.merge(testDf, test2Df, on=['seqname','start','end','strand','gene_id','transcript_id'], how='outer', indicator='merge_check').drop_duplicates()
+    
+    # TODO: check that theres only two in left and right only...
+    allGood = merge[merge['merge_check'] == "both"].drop('merge_check', axis=1)
+    
+    leftOnly = merge[merge['merge_check'] == "left_only"].drop('merge_check', axis=1)
+    rightOnly = merge[merge['merge_check'] == "left_only"].drop('merge_check', axis=1)
+
+    leftOnly[]
+
+    leftOnly = leftOnly.agg({
+        'start':max,
+        'end':max})
+
+
+    masterDf.groupby('transcript_id')
+    for row in masterDf.to_dict('records'):
+        jHash = row['transcript_id']
+        
+        if jHash in ['gtf']
+        
+        
         
 
+    
+    testDf = masterDf[masterDf['transcript_id'] == "000ab5cb90829f133fd4e78f093a2d795bc47c83a7aaef93743560cf7c37f949"]
+    test2Df = gtf[gtf['transcript_id'] == "000ab5cb90829f133fd4e78f093a2d795bc47c83a7aaef93743560cf7c37f949"]
+    
+
+
+    
+    for gtf in gtfDct.values():
+        if masterDf.empty:
+            masterDf = gtf
+        else:
+            
+            # TODO: verify num in both, num left only, num right only is accurate
+            
+            # TODO: verify that this works
+            merge = pd.merge(masterDf, gtf, on=['seqname','strand','start','end','gene_id','transcript_id'], how='outer', indicator='merge_check').drop_duplicates()
+            
+            merge['start'] = merge[['start_x','start_y']].max(axis=1)
+            merge.drop(['start_x','start_y'], inplace=True, axis=1)
+
+            merge['end'] = merge[['end_x','end_y']].max(axis=1)
+            merge.drop(['end_x','end_y'], inplace=True, axis=1)
+            
+            masterDf = merge.drop_duplicates().drop('merge_check', axis = 1)    
+
+    masterDf['transcript_id'].nunique()
+    
+    masterDf.to_csv(index=False)
+    
+    print("Writing GTF...")
+    if os.path.isfile(outGTF):
+        os.remove(outGTF)
+
+    trand.io.write_gtf(data=masterDf, out_fhs={"gtf": "/nfshome/k.bankole/mclab/SHARE/McIntyre_Lab/sex_specific_splicing/one_gene_her_trand_test/test.gtf"}, fh_name="gtf")
+    
     # Create dct {ujc gtf name:GTF DF}
     # indexDfDct = dict()
     # allHashSet = set()
