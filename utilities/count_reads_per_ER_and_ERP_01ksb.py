dde70fc868248d8c063b1cccf0df8509e35c4037
Kinfeosioluwa Bankole <k.bankole@ufgi-grimshawi.ahc.ufl.edu>
2024-07-24 16:10:33 -0400

    finished script that counts number of reads per ERP and ER using UJC count file and ERP/flagER file. needs verification

--- utilities/count_reads_per_ER_and_ERP_01ksb.py
+++ utilities/count_reads_per_ER_and_ERP_01ksb.py
@@ -13,7 +13,7 @@ def getOptions():
         description="Count num reads per ERP/ERR using read counts from "
                     "UJC output and \"ERP\"/\"flagER\" file. For each "
                     "input file, input a list of all desired samples. "
-                    "Output will include counts per ERP split by sample, "
+                    "Output will include counts per ERP/ER split by sample, "
                     "where sample is in the column header."
 
     )
@@ -64,13 +64,21 @@ def getOptions():
         "mapped to. Must match what is in the file name."
     )
 
+    parser.add_argument(
+        "-name",
+        "--name",
+        dest="name",
+        required=True,
+        help="Name for collective samples (ex: dmel_data)."
+    )
+
     # Output data
     parser.add_argument(
         "-o",
-        "--outfile",
-        dest="outFile",
+        "--outdir",
+        dest="outdir",
         required=True,
-        help="Output path and file name."
+        help="Output directory. Must already exist."
     )
 
     parser.add_argument(
@@ -104,15 +112,17 @@ def main():
 
     sampleLst = ["dsan_F", "dsan_M"]
     genome = "dsan1"
-    outFile = ""
+    name = "dsan_data"
+    outdir = ""
     prefix = None
 
     # erpFileLst = args.erpFileLst
     # flagERFileLst = args.flagERFileLst
     # cntFileLst = args.cntFileLst
     # genome = args.genome
+    # name = args.name
     # sampleLst = args.sampleLst
-    # outFile = args.outFile
+    # outdir = args.outdir
     # prefix = args.prefix
 
     alphatic = time.perf_counter()
@@ -211,28 +221,12 @@ def main():
     print(
         f"File read complete! Took {toc-alphatic:0.4f} seconds.")
 
-    test = fileDfDct['dsan_F']
-    erpDf = test[0]
-    erDf = test[1]
-    cntDf = test[2]
-    sample = sampleLst[0]
-
-    erDf['ER'] = erDf['exonRegion']
-
-    mergeDf = pd.merge(erDf, cntDf, on='jxnHash',
-                       how='outer', indicator='merge_check')
-
-    mergeDf['numRead'] = mergeDf.apply(
-        lambda x: x['numTranscripts'] if x['flagER'] == 1 else 0, axis=1)
-
-    mergeDf['numRead'] = mergeDf.apply(
-        lambda x: x['numTranscripts'] if x['flagER'] == 1 else 0, axis=1)
-
-    erCntDf = mergeDf.groupby(['ER', 'geneID'], sort=False).agg({
-        'flagER': sum,
-        'numRead': sum,
-        'strand': set,
-    }).reset_index()
+    # # For testing
+    # test = fileDfDct['dsan_F']
+    # erpDf = test[0]
+    # erDf = test[1]
+    # cntDf = test[2]
+    # sample = sampleLst[0]
 
     allSampleERPCntDf = pd.DataFrame()
     allSampleERCntDf = pd.DataFrame()
@@ -273,12 +267,40 @@ def main():
             erpCntDf['strand'] = erpCntDf['strand'].apply(
                 lambda x: list(x)[0])
 
-        print(f"{sample} counts complete and verified!")
+        print(f"{sample} ERP counts complete and verified!")
 
         # Reorder columns to look nicer
         erpCntDf = erpCntDf[['ERP', 'geneID', 'strand', numReadCol]]
 
         ######### Part 2: Count Reads per exon region (ER) ########
+
+        mergeDf = pd.merge(erDf, cntDf, on='jxnHash',
+                           how='outer', indicator='merge_check')
+        mergeDf[numReadCol] = mergeDf.apply(
+            lambda x: x['numTranscripts'] if x['flagER'] == 1 else 0, axis=1)
+
+        erCntDf = mergeDf.groupby(['ER', 'geneID'], sort=False).agg({
+            'flagER': sum,
+            numReadCol: sum,
+            'strand': set,
+        }).reset_index()
+
+        # TODO: need to figure out a verification step
+
+        singleStrandER = erCntDf['strand'].apply(lambda x: len(x) == 1)
+        if not singleStrandER.all():
+            raise Exception(
+                "There are ERs belonging to more than one strand. Quitting.")
+        else:
+            erCntDf['strand'] = erCntDf['strand'].apply(
+                lambda x: list(x)[0])
+
+        print(f"{sample} ER counts complete and verified!")
+
+        # Reorder columns to look nicer
+        erCntDf = erCntDf[['ER', 'geneID', 'strand', numReadCol]]
+
+        ######### Part 3: Merge samples together (keep samples in separate columns) ########
 
         if allSampleERPCntDf.empty:
             allSampleERPCntDf = erpCntDf
@@ -288,15 +310,55 @@ def main():
 
         readNumColLst = [
             col for col in allSampleERPCntDf.columns if 'numRead' in col]
-        allSampleERPCntDf[readNumColLst] = allSampleERPCntDf[readNumColLst].fillna(
-            0)
+
+        allSampleERPCntDf[readNumColLst] = \
+            allSampleERPCntDf[readNumColLst].fillna(0)
+
+        if allSampleERCntDf.empty:
+            allSampleERCntDf = erCntDf
+        else:
+            allSampleERCntDf = pd.merge(allSampleERCntDf, erCntDf, on=[
+                'ER', 'geneID', 'strand'], how='outer')
+
+        readNumColLst = [
+            col for col in allSampleERCntDf.columns if 'numRead' in col]
+
+        allSampleERCntDf[readNumColLst] = \
+            allSampleERCntDf[readNumColLst].fillna(0)
 
     # TODO: output stuff
     outReadsPerERPDf = allSampleERPCntDf.copy()
+    outReadsPerERDf = allSampleERCntDf.copy()
+
+    if prefix:
+        outPrefix = "{}/{}_".format(outdir, prefix)
+    else:
+        outPrefix = "{}/".format(outdir)
+
+    readsPerERPFile = outPrefix + "{}_2_{}_ERP_cnts.csv".format(name, genome)
+
+    outReadsPerERPDf.to_csv(
+        readsPerERPFile, index=False)
+
+    readsPerERFile = outPrefix + "{}_2_{}_ER_cnts.csv".format(name, genome)
+    outReadsPerERDf.to_csv(
+        readsPerERFile, index=False)
+
+    if erpOnlyHshLst:
+        pd.Series(erpOnlyHshLst).to_csv(
+            outPrefix +
+            "list_{}_2_{}_ERP_only_jxnHash.txt".format(name, genome),
+            index=False, header=False)
+
+    if cntOnlyHshLst:
+        pd.Series(cntOnlyHshLst).to_csv(
+            outPrefix +
+            "list_{}_2_{}_ujc_cnt_only_jxnHash.txt".format(name, genome),
+            index=False, header=False)
 
     omegatoc = time.perf_counter()
 
-    print(f"Complete! Took {(omegatoc-alphatic):0.4f} seconds.")
+    print(f"Process Complete! Took {(omegatoc-alphatic):0.4f} seconds.")
 
 
 if __name__ == '__main__':
