7b4207da93775e0b318504b84c72e4719bc4147f
Kinfeosioluwa Bankole <k.bankole@ufgi-grimshawi.ahc.ufl.edu>
2024-08-02 15:40:34 -0400

    finalized script to output list of jxnHash that are multiGene for ujc output that was run by gene then catted

--- utilities/search_cat_sample_ujc_for_multiGene_jxnHash_01ksb.py
+++ utilities/search_cat_sample_ujc_for_multiGene_jxnHash_01ksb.py
@@ -6,13 +6,24 @@ import pandas as pd
 
 def getOptions():
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(
+        description="Input a \"ujc xscript link\" file that has been run by "
+                    "gene and catted together. Script outputs a list of "
+                    "jxnHash that exist in multiple genes.")
 
     # Input data
-    parser.add_argument("-", "--", dest="", required=True, help="")
+    parser.add_argument("-i",
+                        "--infile",
+                        dest="inFile",
+                        required=True,
+                        help="Path to input file.")
 
     # Output data
-    parser.add_argument("-", "--", dest="", required=True, help="")
+    parser.add_argument("-o",
+                        "--outfile",
+                        dest="outFile",
+                        required=True,
+                        help="Full path for output file.")
 
     args = parser.parse_args()
     return args
@@ -20,20 +31,38 @@ def getOptions():
 
 def main():
 
-    inFile = ""
+    inFile = "/nfshome/k.bankole/mnt/exasmb.rc.ufl.edu-blue/mcintyre/share/transcript_ortholog/ujc_species_gtf_combined_genes/dsan_data_2_dsan1_ujc_xscript_link.csv"
+
+    outFile = "/nfshome/k.bankole/Desktop/test_folder/multigene.txt"
+
+    inFile = args.inFile
+    outFile = args.outFile
 
     inDf = pd.read_csv(inFile, low_memory=False)
 
     hash2GeneDf = inDf[['jxnHash', 'geneID']]
 
-    hash2GeneDf = hash2GeneDf.groupby(
+    print("Num total rows:", len(hash2GeneDf['jxnHash']))
+
+    uniqHashDf = hash2GeneDf.groupby(
         'jxnHash').agg({'geneID': set}).reset_index()
 
-    hash2GeneDf['multiGene'] = hash2GeneDf['geneID'].apply(
+    print("Num unique jxnHash:", len(uniqHashDf))
+
+    uniqHashDf['multiGene'] = uniqHashDf['geneID'].apply(
         lambda x: len(x) > 1)
 
-    multiGeneDf = hash2GeneDf[hash2GeneDf['multiGene']]
-    singleGeneDf = hash2GeneDf[~hash2GeneDf['multiGene']]
+    multiGeneDf = uniqHashDf[uniqHashDf['multiGene']]
+    # singleGeneDf = uniqHashDf[~uniqHashDf['multiGene']]
+
+    pctMultiGene = len(multiGeneDf) / len(uniqHashDf)
+
+    print("Num multiGene jxnHash:", len(multiGeneDf))
+    print(f"Percent of all jxnHash that are multiGene: {pctMultiGene:%}")
+
+    outDf = multiGeneDf['jxnHash']
+
+    outDf.to_csv(outFile, index=False, header=False)
 
 
 if __name__ == '__main__':
