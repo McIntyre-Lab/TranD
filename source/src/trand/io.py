84cbfa8d82b8f2bb08f9b783b4ccab29602bbd4e
Kinfeosioluwa Bankole <k.bankole@ufgi-grimshawi.ahc.ufl.edu>
2024-08-20 12:14:34 -0400

    removed some redundant scripts and changed tabbing

--- source/src/trand/io.py
+++ source/src/trand/io.py
@@ -35,8 +35,7 @@ def open_output_files(outdir, outfiles):
     try:
         for outfile in outfiles:
             fh = Path(outdir) / outfiles[outfile]
-                            
-                    
+
             out_fhs[outfile] = fh
     except SystemError as e:
         logger.error("Cannot open one or more output file for writing: {}", e)
@@ -57,7 +56,8 @@ def get_gtf_attribute(transcript_id, gene_id):
 #                 for line in txtLst:
 #                         f.write("{:<20} {:<20}\n".format(str(line[0]), str(line[1])))
 #                         # f.write((txtLst[0] + "{:<5} *2 " txtlst[1]).format(*row))
-        
+
+
 def write_gtf(data, out_fhs, fh_name):
     """Write output gtf files."""
     if len(data) == 0:
@@ -140,132 +140,134 @@ def write_gtf(data, out_fhs, fh_name):
 #     return data
 
 def read_exon_data_from_file(infile, keepSrc=False):
-        """
-        Create a pandas dataframe with exon records from a gtf file
-        Raw gtf data:
-        seqname source  feature  start end  score strand frame attributes
-        2L FlyBase 5UTR  7529  7679 . +  . gene_symbol "CG11023"; transcript_id "FBtr...
-        2L FlyBase exon  7529  8116 . +  . gene_symbol "CG11023"; transcript_id "FBtr...
+    """
+    Create a pandas dataframe with exon records from a gtf file
+    Raw gtf data:
+    seqname source  feature  start end  score strand frame attributes
+    2L FlyBase 5UTR  7529  7679 . +  . gene_symbol "CG11023"; transcript_id "FBtr...
+    2L FlyBase exon  7529  8116 . +  . gene_symbol "CG11023"; transcript_id "FBtr...
 
-        Exon Fragment Data for Event Analysis:
-        seqname start end   strand gene_id      transcript_id
-        2L      7529  8116  +      FBgn0031208  FBtr0300689
-        2L      8193  9484  +      FBgn0031208  FBtr0300689
-        2L      9839  11344 -      FBgn0002121  FBtr0078169
-        """
-        
-        print ("Reading GTF...")
+    Exon Fragment Data for Event Analysis:
+    seqname start end   strand gene_id      transcript_id
+    2L      7529  8116  +      FBgn0031208  FBtr0300689
+    2L      8193  9484  +      FBgn0031208  FBtr0300689
+    2L      9839  11344 -      FBgn0002121  FBtr0078169
+    """
 
-        omegatic = time.perf_counter()
+    print("Reading GTF...")
 
-        all_gtf_columns = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame',
-                           'attributes', 'comments']
-        
-        drop_columns = ['feature', 'score', 'frame', 'comments']
-        # drop_columns = ['source', 'feature', 'score', 'frame', 'comments']
+    omegatic = time.perf_counter()
 
-        data = pd.read_csv(infile, sep='\t', comment='#', header=None, low_memory=False)
-        file_cols = data.columns
-        
-        if len(file_cols) < len(all_gtf_columns):
-            gtf_cols = all_gtf_columns[:len(file_cols)]
-        data.columns = gtf_cols
-        drop_cols = [x for x in drop_columns if x in gtf_cols]
-        
-        data = data[data['feature'] == 'exon']
-        data = data.drop(labels=drop_cols, axis=1)
+    all_gtf_columns = ['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame',
+                       'attributes', 'comments']
 
-        data['source'] = data['source'].astype(str)
-        data['seqname'] = data['seqname'].astype(str)
-        data['start'] = data['start'].astype(int)
-        data['end'] = data['end'].astype(int)
-        
-        data.reset_index(drop=True, inplace=True)
-        
-        sourceLst = []
-        seqnameLst = []
-        startLst = []
-        endLst = []
-        strandLst = []
-        geneIDLst = []
-        xscriptIDLst = []
-        
-        for row in data.to_dict('records'):
-                rawAttr = row['attributes']
-                attrLst = [x.strip() for x in rawAttr.strip().split(';')]
-                gnTrAttr = [x for x in attrLst if 'transcript_id' in x or 'gene_id' in x]
-                gene_id, transcript_id = None, None
-                
-                for item in gnTrAttr:
-                        if 'gene_id' in item:
-                                gene_id = item.split('gene_id')[1].strip().strip('\"')
-                        elif 'transcript_id' in item:
-                                transcript_id = item.split('transcript_id')[-1].strip().strip('\"')
-                                
-                if not gene_id:
-                        print("gene_id not found in '{}'", row)
-                        gene_id = None
-                        
-                if not transcript_id:
-                        print("transcript_id not found in '{}'", row)
-                        transcript_id = None
+    drop_columns = ['feature', 'score', 'frame', 'comments']
+    # drop_columns = ['source', 'feature', 'score', 'frame', 'comments']
 
-                sourceLst.append(row['source'])
-                seqnameLst.append(row['seqname'])
-                startLst.append(row['start'])
-                endLst.append(row['end'])
-                strandLst.append(row['strand'])
-                
-                geneIDLst.append(gene_id)
-                xscriptIDLst.append(transcript_id)
-                
-        
-        if keepSrc:
-            newData = pd.DataFrame(
-                    {
-                            'seqname':seqnameLst,
-                            'source':sourceLst,
-                            'start':startLst,
-                            'end':endLst,
-                            'strand':strandLst,
-                            'gene_id':geneIDLst,
-                            'transcript_id':xscriptIDLst
-                    })
-        else:
-            
-            newData = pd.DataFrame(
-                    {
-                            'seqname':seqnameLst,
-                            'start':startLst,
-                            'end':endLst,
-                            'strand':strandLst,
-                            'gene_id':geneIDLst,
-                            'transcript_id':xscriptIDLst
-                    })
-                
-        logger.debug("Exon data rows: {}", newData.shape[0])
-        
-        missing_value_num = newData.isnull().sum().sum()
-        if missing_value_num > 0:
-                logger.warning("Total number of missing values: {}", missing_value_num)
-        else:
-                logger.info("No missing values in data")
-        
-        gene_id_missing_value_num = newData['gene_id'].isnull().sum()
-        
-        transcript_id_missing_value_num = newData['transcript_id'].isnull().sum()
-        
-        if gene_id_missing_value_num > 0:
-                logger.warning("Missing gene_id value number: {}", gene_id_missing_value_num)
-        if transcript_id_missing_value_num > 0:
-                logger.warning("Missing transcript_id value number: {}", transcript_id_missing_value_num)
-        
-                
-        newData['start'] = pd.to_numeric(newData['start'], downcast="unsigned")
-        newData['end'] = pd.to_numeric(newData['end'], downcast="unsigned")
-        
-        toc = time.perf_counter()       
-        
-        print(f"GTF Read complete,  took {toc-omegatic:0.4f} seconds.")
-        return newData
+    data = pd.read_csv(infile, sep='\t', comment='#',
+                       header=None, low_memory=False)
+    file_cols = data.columns
 
+    if len(file_cols) < len(all_gtf_columns):
+        gtf_cols = all_gtf_columns[:len(file_cols)]
+    data.columns = gtf_cols
+    drop_cols = [x for x in drop_columns if x in gtf_cols]
+
+    data = data[data['feature'] == 'exon']
+    data = data.drop(labels=drop_cols, axis=1)
+
+    data['source'] = data['source'].astype(str)
+    data['seqname'] = data['seqname'].astype(str)
+    data['start'] = data['start'].astype(int)
+    data['end'] = data['end'].astype(int)
+
+    data.reset_index(drop=True, inplace=True)
+
+    sourceLst = []
+    seqnameLst = []
+    startLst = []
+    endLst = []
+    strandLst = []
+    geneIDLst = []
+    xscriptIDLst = []
+
+    for row in data.to_dict('records'):
+        rawAttr = row['attributes']
+        attrLst = [x.strip() for x in rawAttr.strip().split(';')]
+        gnTrAttr = [
+            x for x in attrLst if 'transcript_id' in x or 'gene_id' in x]
+        gene_id, transcript_id = None, None
+
+        for item in gnTrAttr:
+            if 'gene_id' in item:
+                gene_id = item.split('gene_id')[1].strip().strip('\"')
+            elif 'transcript_id' in item:
+                transcript_id = item.split(
+                    'transcript_id')[-1].strip().strip('\"')
+
+        if not gene_id:
+            print("gene_id not found in '{}'", row)
+            gene_id = None
+
+        if not transcript_id:
+            print("transcript_id not found in '{}'", row)
+            transcript_id = None
+
+        sourceLst.append(row['source'])
+        seqnameLst.append(row['seqname'])
+        startLst.append(row['start'])
+        endLst.append(row['end'])
+        strandLst.append(row['strand'])
+
+        geneIDLst.append(gene_id)
+        xscriptIDLst.append(transcript_id)
+
+    if keepSrc:
+        newData = pd.DataFrame(
+            {
+                'seqname': seqnameLst,
+                'source': sourceLst,
+                'start': startLst,
+                'end': endLst,
+                'strand': strandLst,
+                'gene_id': geneIDLst,
+                'transcript_id': xscriptIDLst
+            })
+    else:
+
+        newData = pd.DataFrame(
+            {
+                'seqname': seqnameLst,
+                'start': startLst,
+                'end': endLst,
+                'strand': strandLst,
+                'gene_id': geneIDLst,
+                'transcript_id': xscriptIDLst
+            })
+
+    logger.debug("Exon data rows: {}", newData.shape[0])
+
+    missing_value_num = newData.isnull().sum().sum()
+    if missing_value_num > 0:
+        logger.warning("Total number of missing values: {}", missing_value_num)
+    else:
+        logger.info("No missing values in data")
+
+    gene_id_missing_value_num = newData['gene_id'].isnull().sum()
+
+    transcript_id_missing_value_num = newData['transcript_id'].isnull().sum()
+
+    if gene_id_missing_value_num > 0:
+        logger.warning("Missing gene_id value number: {}",
+                       gene_id_missing_value_num)
+    if transcript_id_missing_value_num > 0:
+        logger.warning("Missing transcript_id value number: {}",
+                       transcript_id_missing_value_num)
+
+    newData['start'] = pd.to_numeric(newData['start'], downcast="unsigned")
+    newData['end'] = pd.to_numeric(newData['end'], downcast="unsigned")
+
+    toc = time.perf_counter()
+
+    print(f"GTF Read complete,  took {toc-omegatic:0.4f} seconds.")
+    return newData
